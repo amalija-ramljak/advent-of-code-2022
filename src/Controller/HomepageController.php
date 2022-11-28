c63d4e32087dbbd4ce5ed4d388d9bb9035f54e2f
amalija-ramljak <amalija@netgen.io>
2022-11-28 22:51:22 +0100

    [init] make homepage dynamic

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Finder\Finder;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,6 +12,10 @@ class HomepageController extends AbstractController
     #[Route('/', 'homepage')]
     public function __invoke(): Response
     {
-        return $this->render('routes/homepage.html.twig', ['days' => range(1, 1)]);
+        $finder = new Finder();
+        $projectDir = $this->getParameter('kernel.project_dir');
+        $solutionServicesCount = $finder->files()->in("${projectDir}/src/Service/SolutionService")->count() - 1;
+
+        return $this->render('routes/homepage.html.twig', ['days' => range(1, $solutionServicesCount)]);
     }
 }
